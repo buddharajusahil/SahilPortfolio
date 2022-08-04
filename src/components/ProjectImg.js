f7b8a6d40221aaa0a4101f05f0ed83dbe1eb7c48
buddharajusahil <61558528+buddharajusahil@users.noreply.github.com>
2022-08-03 19:18:13 -0700

    added project background and image component

---
+++ src/components/ProjectImg.js
@@ -0,0 +1,16 @@
+import "./ProjectImgStyles.css";
+
+import React from 'react'
+
+const ProjectImg = () => {
+  return (
+    <div className = "project-image">
+        <div className = "heading">
+            <h1>This is a heading</h1>
+            <p>This is text</p>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectImg
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ProjectImg from "../components/ProjectImg"
 
 const Projects = () => {
   return (
     <div>
       <Navbar/>
+      <ProjectImg/>
       <Footer/>
     </div>
   )
